0f09fc432bb79ae5d52e633c371568dcf1d46e88
laney522 <pm72222@gmail.com>
2022-01-31 12:38:41 +0900

    22.01.31 add _app.js & Head commit

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import 'antd/dist/antd.css'
 
 const AvoCode = ({ Component }) => {
   return (
+    <>
+    <Head>
+      <meta charSet='utf-8' />
+      <title>AvoCode</title>
+    </Head>
     <Component />
+    </>
   )
 };
 
